ec11384bab993577a6cf177b4ae87d634a25dc59
shagors <shajjadshagor@gmil.com>
2022-03-28 14:59:48 +0600

    spring io check animation add

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import {
   useSpring,
   animated
 } from 'react-spring'
+import { useState } from 'react';
 
 function App() {
+  const [flip, set] = useState(false);
 
   const props = useSpring({
     to: {
@@ -14,7 +16,11 @@ function App() {
     },
     from: {
       opacity: 0
-    }
+    },
+    reset: true,
+      reverse: flip,
+      delay: 200,
+      onRest: () => set(!flip),
   })
   return (
     <div className="App">
